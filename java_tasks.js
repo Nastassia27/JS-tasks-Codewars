4ad38cb82a37c939f5aabc2ed2108dea7cd96b85
Nastassia Derugo <anastasia.derugo@gmail.com>
2023-01-29 22:57:49 +0300

    Using a Temporary Variable

--- java_tasks.js
+++ java_tasks.js
@@ -7,7 +7,7 @@ var max = function(list){
     let n = Math.max.apply(Math, list);
     return n;
 }
-console.log(min([6,8,3,2]));*/
+console.log(min([6,8,3,2]));
 function grow(x){
     return x.reduce((a, b)=> a * b,1);
   }
@@ -20,4 +20,21 @@ let b = "John" + " " + "Doe";
 document.getElementById("demo").innerHTML = b;
 
 let c = "Volvo" + 16 + 4;
-document.getElementById("demo").innerHTML = c;
+document.getElementById("demo").innerHTML = c;*/
+
+//JavaScript program to swap two variables
+
+//take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//create a temporary variable
+let temp;
+
+//swap variables
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
