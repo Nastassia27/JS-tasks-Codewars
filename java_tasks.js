47065fd6fd4b8f2d819f753ad6fd9deb15172145
Nastassia Derugo <anastasia.derugo@gmail.com>
2023-01-29 22:44:56 +0300

    reduce but grow

--- java_tasks.js
+++ java_tasks.js
@@ -1,4 +1,4 @@
-var min = function(list){
+/*var min = function(list){
     let m = Math.min.apply(Math, list);
     return m;
 }
@@ -7,4 +7,8 @@ var max = function(list){
     let n = Math.max.apply(Math, list);
     return n;
 }
-console.log(min([6,8,3,2]));
+console.log(min([6,8,3,2]));*/
+function grow(x){
+    return x.reduce((a, b)=> a * b,1);
+  }
+  console.log(grow([4,6,7,8]));
